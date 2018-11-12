3b06672d7171eff7b45cea0920af2b7222a5dc00
Unknown <mathias.caelenberghe@student.howest.be>
2018-11-12 22:03:05 +0100

    added background stil need to make it paralax

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,11 +21,37 @@ let cursors;
 let player;
 function preload ()
 {
+    this.load.image("layer10","assets/sprites/background/Layer_0010_1.png");
+    this.load.image("layer9","assets/sprites/background/Layer_0009_2.png");
+    this.load.image("layer8","assets/sprites/background/Layer_0008_3.png");
+    this.load.image("layer7","assets/sprites/background/Layer_0007_Lights.png");
+    this.load.image("layer6","assets/sprites/background/Layer_0006_4.png");
+    this.load.image("layer5","assets/sprites/background/Layer_0005_5.png");
+    this.load.image("layer4","assets/sprites/background/Layer_0004_Lights.png");
+    this.load.image("layer3","assets/sprites/background/Layer_0003_6.png");
+    this.load.image("layer2","assets/sprites/background/Layer_0002_7.png");
+    this.load.image("layer1","assets/sprites/background/Layer_0001_8.png");
+    this.load.image("layer0","assets/sprites/background/Layer_0000_9.png");
+
+
+
+
     this.load.spritesheet("dude","assets/character.png",{frameWidth:23,frameHeight:37})
 }
 
 function create ()
 {
+    this.add.image(400, 200, 'layer10');
+    this.add.image(400, 200, 'layer9');
+    this.add.image(400, 200, 'layer8');
+    this.add.image(400, 200, 'layer7');
+    this.add.image(400, 200, 'layer6');
+    this.add.image(400, 200, 'layer5');
+    this.add.image(400, 200, 'layer4');
+    this.add.image(400, 200, 'layer3');
+    this.add.image(400, 200, 'layer2');
+    this.add.image(400, 200, 'layer1');
+    this.add.image(400, 200, 'layer0');
     player = this.physics.add.sprite(100, 450, 'dude');
     player.setScale(2);
     player.setBounce(0.2);
